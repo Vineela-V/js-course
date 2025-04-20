69381a95570d2d795e48e58d003e8bcabc24f70c
vineela <vineelav2000@gmail.com>
2025-04-20 19:12:30 +0530

    com 1

---
+++ datatypes.js
@@ -0,0 +1,102 @@
+/*
+let name = "Vineela";
+alert(`hello, ${name}!`)
+alert(`the result is ${2+3}`)
+
+
+alert(`the result is ${2+3}`);
+
+let isGreater = 4>1.2;
+alert (isGreater);
+
+let age = 24;
+age= undefined;
+alert(age);
+
+let name = "Vinni";
+alert( `hello ${1}`);
+alert (`hello ${"name"}`);
+alert (`hello ${name}`);
+
+let age = prompt('How old are you?', 100);
+alert(`You are ${age} years old!`); 
+
+let isOwner = confirm("Are you the boss?");
+alert(isOwner);
+
+let name = prompt("What is your name?", "");
+alert(name);
+
+let value = false;
+alert(typeof value);
+value = String(value);
+alert(typeof value);
+
+let str = "012";
+alert(typeof str);
+
+let num = Number(str);
+alert(typeof num);
+
+alert(Number(" 123 "));
+alert(Number ("123z"));
+alert(Number(true));
+alert(Number(true));
+
+let x =1;
+x=-x;
+alert(x);
+
+let x=1, y=8;
+alert(x-y);
+alert(8%3);
+
+alert(4 ** (1/2));
+
+let apples ="5";
+let kiwis ="9";
+alert(+apples + +kiwis)
+
+let a=1;
+let b=2;
+let c= 5-(a=b+1)
+alert(a);
+alert(c);
+
+let a,b,c;
+a=b=c=2+1;
+alert(a);
+alert(b);
+alert(c);
+
+let n=2;
+n += 5;
+n *=2;
+alert(n);
+
+let a =1, b=1;
+alert(++a);
+alert(b++);
+
+alert(a);
+alert(b);
+
+let a ="1";
+let b ="2";
+alert(a + b);
+
+let a = +prompt('a?', '');
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+         alert(1);
+         break;
+    case 2:
+    case 3:
+    case 4:
+        alert( '2,3,4');
+        break;
+}
+*/
---
+++ var.js
@@ -0,0 +1,18 @@
+/*
+const COLOR_RED = "#A10";
+const COLOR_GREEN= "#A110";
+const COLOR_BLUE = "#A001";
+const COLOR_YELLOW = "#A1010";
+const COLOR_BLACK = "#A0001";
+
+let color = COLOR_YELLOW;
+alert(color);
+*/
+
+/* let admin, name;
+name = "Vineela";
+admin = name;
+alert(admin); */
+
+
+
