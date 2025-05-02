678c31d86942af2c854dad05f40a43f657e18b97
vineela <vineelav2000@gmail.com>
2025-05-02 15:10:08 +0530

    com 9

--- Api,Async.js
+++ Api,Async.js
@@ -108,7 +108,6 @@ async function doWorks() {
 }
 
 doWorks()
-*/
 
 function pseudoRandom(seed) {
   let value = seed;
@@ -122,3 +121,101 @@ let generator = pseudoRandom(1);
 alert(generator()); 
 alert(generator()); 
 alert(generator());
+*/
+
+// callback fun //
+/*
+function greet( name, callback) {
+  console.log("Hello" + name);
+  callback();
+}
+function sayBye() {
+  console.log("Goodbye!");
+}
+greet("Vineela", sayBye);
+
+console.log("Start");
+setTimeout(function() {
+  console.log("Inside setTimeout");
+}, 3000);
+console.log("End");
+
+
+// callbacks in fun //
+function calc(a, b, callback) {
+  return callback(a, b);
+}
+function add(x, y) {
+  return x + y;
+}
+function mul(x, y) {
+  return x * y;
+}
+console.log(calc(4,5, add));
+console.log(calc(7,6, mul));
+
+
+// callbacks in event listners//
+document.getElementById("Button").addEventListener("click", function () {
+  console.log("Button clicked!");
+});
+
+// callbacks in api calls//
+function fetch(callback) {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(response => response.json())
+  .then(data => callback(data))
+  .catch(error => console.error("Error:" , error));
+}
+function handle(data) {
+  console.log("Fetched Data:", data);
+}
+fetch(handle);
+
+let checkEven = new Promise((resolve, reject) =>{
+  let number = 5;
+  if (number % 2 === 0) resolve("The number is even!");
+  else reject("The number is odd!");
+});
+checkEven
+   .then((message) => console.log(message))
+   .catch((error) => console.log(error));
+
+// async & await in js//
+async function fetchData() {
+  const response = await
+  fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  console.log(data);
+}
+fetchData();
+
+// async fun//
+const getData = async() => {
+  let data = "Heyy, Hello";
+  return data;
+}
+getData().then(data => console.log(data));
+
+//await keyword//
+const getData = async () => {
+   let y = await "Hello Hii";
+   console.log(y);
+}
+console.log(1);
+getData();
+console.log(2);
+*/
+
+// error handling in async/await//
+
+async function fetchData() {
+  try {
+    let response = await
+    fetch('https://api.example.com/data');
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
